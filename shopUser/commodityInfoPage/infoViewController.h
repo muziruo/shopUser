b9aea299b13631ced77c6abeaf466d0a455b571d
muziruo <1149354821@qq.com>
2019-04-19 11:44:04 +0800

    commodityInfo update

---
+++ shopUser/commodityInfoPage/infoViewController.h
@@ -0,0 +1,17 @@
+//
+//  infoViewController.h
+//  shopUser
+//
+//  Created by 李祎喆 on 2019/4/19.
+//  Copyright © 2019 李祎喆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface infoViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
