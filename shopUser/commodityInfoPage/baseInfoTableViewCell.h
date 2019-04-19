7493dcec1f396954627840f84283974df08d29ea
muziruo <1149354821@qq.com>
2019-04-19 11:19:52 +0800

    commodityInfo update

---
+++ shopUser/commodityInfoPage/baseInfoTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  baseInfoTableViewCell.h
+//  shopUser
+//
+//  Created by 李祎喆 on 2019/4/19.
+//  Copyright © 2019 李祎喆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface baseInfoTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ shopUser/commodityInfoPage/baseInfoTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  baseInfoTableViewCell.m
+//  shopUser
+//
+//  Created by 李祎喆 on 2019/4/19.
+//  Copyright © 2019 李祎喆. All rights reserved.
+//
+
+#import "baseInfoTableViewCell.h"
+
+@implementation baseInfoTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
